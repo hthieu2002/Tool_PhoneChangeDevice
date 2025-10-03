01258c3f8df2caae67fc8521a1301cedd7850877
doanvtamhuynh <huynhdoan.2132@gmail.com>
2025-10-03 11:03:42 +0700

    update new setprop for gms

--- ToolChange/Models/PifData.cs
+++ ToolChange/Models/PifData.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeepDroid.Models
+﻿namespace DeepDroid.Models
 {
     internal class PifData
     {
         public string FINGERPRINT { get; set; }
+        public string MANUFACTURER { get; set; } = "Google";
+        public string MODEL { get; set; } = "Pixel";
+        public string RELEASE { get; set; } = "";
+        public string SDK_INT { get; set; } = "32";
         public string SECURITY_PATCH { get; set; }
-        public string DEVICE_INITIAL_SDK_INT { get; set; }
+        public string DEVICE_INITIAL_SDK_INT { get; set; } = "21";
     }
 }
