946cae27515c167643344e64dc89585feeafa89e
doanvtamhuynh <huynhdoan.2132@gmail.com>
2025-12-17 09:44:51 +0700

    Util: bring back some spoof

--- ToolChange/Services/Util.cs
+++ ToolChange/Services/Util.cs
@@ -235,7 +235,11 @@ namespace ToolChange.Services
                     ADBService.deleteSetting("bluetooth_address", deviceId, "secure");
                     ADBService.deleteSetting("bluetooth_name", deviceId, "secure");
                     ADBService.deleteSetting("device_name", deviceId);
-                    //ADBService.putSetting("non_persistent_mac_randomization_force_enabled", "1", deviceId);
+                    ADBService.putSetting("android_id", tempDevice.AndroidId, deviceId, "secure");
+                    ADBService.putSetting("bluetooth_address", tempDevice.BlueToothMacAddress, deviceId, "secure");
+                    ADBService.putSetting("bluetooth_name", bluetoothName, deviceId, "secure");
+                    ADBService.putSetting("device_name", deviceName, deviceId);
+                    ADBService.putSetting("non_persistent_mac_randomization_force_enabled", "1", deviceId);
                     ADBService.putSetting("screen_off_timeout", "1800000", deviceId, "system");
                     ADBService.runCMDRoot("shell locksettings set-disabled true", deviceId);
 
